02b25c4a914833094ff3b86cd97511ebdfafe24f
Tom Pesman <tom@tnux.net>
2012-09-25 10:39:14 +0200

    the message is not delivered, so raise also for InvalidRegistration and NotRegistered errors

--- lib/push/message_gcm.rb
+++ lib/push/message_gcm.rb
@@ -26,7 +26,6 @@ module Push
 
     def check_for_error(connection)
       response = connection.response
-
       if response.code.eql? "200"
         hsh = MultiJson.load(response.body)
         if hsh["failure"] == 1
@@ -41,10 +40,10 @@ module Push
               Push::FeedbackGcm.create!(:app => connection.provider.configuration[:name], :failed_at => Time.now,
                 :device => device, :follow_up => 'delete')
             end
-          else
-            Push::Daemon.logger.error("[#{connection.name}] Error received.")
-            raise Push::DeliveryError.new(response.code, id, msg, "GCM")
           end
+
+          Push::Daemon.logger.error("[#{connection.name}] Error received.")
+          raise Push::DeliveryError.new(response.code, id, msg, "GCM")
         elsif hsh["canonical_ids"] == 1
           # success, but update device token
           update_to = hsh["results"][0]["registration_id"]
