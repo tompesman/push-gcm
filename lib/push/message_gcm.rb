df07cfdfc15a65d374bc0a4f6515dd3fe99db749
Tom Pesman <tom@tnux.net>
2013-02-15 09:47:56 +0100

    Collapse_key is an optional parameter. Fixes #1.

--- lib/push/message_gcm.rb
+++ lib/push/message_gcm.rb
@@ -1,6 +1,5 @@
 module Push
   class MessageGcm < Push::Message
-    validates :collapse_key, :presence => true
     # TODO: validates max size -> The message size limit is 4096 bytes.
     # The total size of the payload data that is included in a message can't exceed 4096 bytes.
     # Note that this includes both the size of the keys as well as the values.
@@ -11,7 +10,7 @@ module Push
     def to_message
       hsh = Hash.new
       hsh['registration_ids'] = [device]
-      hsh['collapse_key'] = collapse_key
+      hsh['collapse_key'] = collapse_key if collapse_key
       hsh['delay_when_idle'] = delay_when_idle if delay_when_idle
       hsh['time_to_live'] = time_to_live if time_to_live
       hsh['data'] = payload
